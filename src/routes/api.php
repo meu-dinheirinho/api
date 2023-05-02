71d35e4c70b234ad675b0aa05669407f0a33b8bc
Elquiane Lima <elquianelima23@gmail.com>
2023-05-02 16:41:31 -0300

    laravel-sanctum (login, register and logout) without tests

--- src/routes/api.php
+++ src/routes/api.php
@@ -15,13 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-], function ($router) {
-    Route::post('login', 'AuthController@login');
-    Route::post('logout', 'AuthController@logout');
-    Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
+// Route::group([
+//     'middleware' => 'api',
+//     'prefix' => 'auth'
+// ], function ($router) {
+//     Route::post('login', 'AuthController@login');
+//     Route::post('logout', 'AuthController@logout');
+//     Route::post('refresh', 'AuthController@refresh');
+//     Route::post('me', 'AuthController@me');
+// });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
 
+
+Route::prefix('auth')->group(function() {
+    Route::post('login', [\App\Http\Controllers\Auth\Api\LoginController::class, 'login']);
+    Route::post('logout', [\App\Http\Controllers\Auth\Api\LoginController::class, 'logout'])->middleware('auth:sanctum');
+    Route::post('register', [\App\Http\Controllers\Auth\Api\RegisterController::class, 'register']);
+});
